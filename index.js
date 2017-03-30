5098aeb215f0cb26e9bb9ef4b70650e6241e0aad
Alberto Gimeno <gimenete@gmail.com>
2017-03-30 21:01:05 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+
+const express = require('express')
+const authenticationService = require('pnp-authentication-service')
+const jwt = require('./jwt')()
+
+exports.startServer = (config, callback) => {
+  const port = process.env.PORT || 3000
+  const app = express()
+  app.set('view engine', 'pug')
+  app.set('views', './views')
+
+  const router = authenticationService.createRouter({})
+  app.use('/auth', router)
+
+  app.get('/', (req, res, next) => {
+    res.render('index.pug')
+  })
+
+  app.get('/callback', (req, res, next) => {
+    jwt.verify(req.query.jwt)
+      .then(data => {
+        res.render('callback.pug', data)
+      })
+      .catch(next)
+  })
+
+  return app.listen(port, callback)
+}
+
+if (require.main === module) {
+  const server = exports.startServer({}, () => {
+    const port = server.address().port
+    console.log(`Listening on port ${port}! Visit http://127.0.0.1:${port}/auth`)
+  })
+}
